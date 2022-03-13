de6c0765a12a5105e0eb40b3b3043658c5ab7a9e
Kostya Grishko <grishko.konstantins@gmail.com>
2022-03-13 20:35:09 +0300

    final fix

--- Navigation/Navigation/ProfileView.swift
+++ Navigation/Navigation/ProfileView.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import UIKit
+
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var siteLabel: UILabel!
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var textView: UITextView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.commonInit()
+    }
+    
+    private func commonInit() {
+        let bundle = Bundle.init(for: ProfileView.self)
+        if let viewToAdd = bundle.loadNibNamed("ProfileView", owner: self, options: nil), let contentView = viewToAdd.first as? UIView {
+            addSubview(contentView)
+            contentView.frame = self.bounds
+            contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+            }
+    }
+}
--- Navigation/Navigation/ProfileViewController.swift
+++ Navigation/Navigation/ProfileViewController.swift
@@ -5,4 +5,13 @@
 //  Created by Константин Гришко on 12.03.2022.
 //
 
-import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profileView: ProfileView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
